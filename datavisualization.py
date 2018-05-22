05b1892e9f272e331a5742840a1b083fe9c958a0
AstridKery <ckery1@umbc.edu>
2018-05-22 12:40:14 -0400

    changed titles for graphs

--- datavisualization.py
+++ datavisualization.py
@@ -3,12 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-def generateHist(data, bins,title):
+def generateHist(data, bins, title, xlabel, ylabel):
 
     plt.hist(data, bins, histtype='bar', rwidth=0.7)
 
-    plt.xlabel("normalized scale")
-    plt.ylabel("num users")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.title(title)
     plt.legend()
     plt.show()
@@ -21,7 +21,7 @@ def getClusterStatistics(x_input):
 
     tmp = 0
     for i in x_input:
-        tmp = (i - mean)**2
+        tmp = (i - mean) ** 2
 
     variance = tmp/(len(x_input)-1)
     print(
@@ -32,18 +32,33 @@ def getClusterStatistics(x_input):
 
 
 def getStats(filename, data_file, title = "data"):
-    columns = ['num_states','state_variance','1_day_max_reviews','star_variance']
 
     if len(filename) > 0:
         data_file = pd.read_csv(filename)
 
     results  = {}
 
-    for named_col in columns:
-        results[named_col] = getClusterStatistics(data_file[named_col])
+    dataset = "Dataset 1"
 
-        bins = np.sort(list(data_file[named_col].unique()))
-        generateHist(data_file[named_col],bins, title+":"+named_col)
+    col_name = 'num_states'
+    results[col_name] = getClusterStatistics(data_file[col_name])
+    bins = np.sort(list(data_file[col_name].unique()))
+    generateHist(data_file[col_name], bins, "Number of States, " + dataset, "Number of States Normalized", "Number of Users")
+    
+    col_name = 'state_variance'
+    results[col_name] = getClusterStatistics(data_file[col_name])
+    bins = np.sort(list(data_file[col_name].unique()))
+    generateHist(data_file[col_name], bins, "State Variance, " + dataset, "Variance Normalized", "Number of Users")
+    
+    col_name = '1_day_max_reviews'
+    results[col_name] = getClusterStatistics(data_file[col_name])
+    bins = np.sort(list(data_file[col_name].unique()))
+    generateHist(data_file[col_name], bins, "Max Reviews in a Day, " + dataset, "Number of Reviews Normalized", "Number of Users")
+    
+    col_name = 'star_variance'
+    results[col_name] = getClusterStatistics(data_file[col_name])
+    bins = np.sort(list(data_file[col_name].unique()))
+    generateHist(data_file[col_name], bins, "Star Variance, " + dataset, "Number of Stars Normalized", "Number of Users")
 
     print(results)
 
